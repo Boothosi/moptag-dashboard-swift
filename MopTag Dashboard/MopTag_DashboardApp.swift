489df316c6e683f1ffd507e68a36943a561b71ac
Ennio Italiano <e.italiano@vargroup.it>
2024-11-09 07:22:36 +0100

    Initial Commit

---
+++ MopTag Dashboard/MopTag_DashboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  MopTag_DashboardApp.swift
+//  MopTag Dashboard
+//
+//  Created by Ennio Italiano on 09/11/24.
+//
+
+import SwiftUI
+
+@main
+struct MopTag_DashboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
